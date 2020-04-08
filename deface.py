278bdbbcc665dbe42bd6c040122a54666cf7f86a
Hagirs19 <60696197+Hagirs19@users.noreply.github.com>
2020-04-07 23:14:18 -0700

    Update deface.py

--- deface.py
+++ deface.py
@@ -6,7 +6,6 @@ figlet "HACKED BY R3XD"|lolcat
 echo -e "\e[32mCreated By Admin_H\e[0m"
 echo -e "\e[31mNo One Cares But\e[0m"
 echo -e "\e[31mWe cares about You\e[0m"
-echo -e "\e[35Testting Doang Kok :v\e[0m"
 sleep 2
 clear
 mulai
