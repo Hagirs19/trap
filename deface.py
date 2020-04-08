06d68a24de493fe4483d84545ee7f99b377e128a
Hagirs19 <60696197+Hagirs19@users.noreply.github.com>
2020-04-08 01:23:08 -0700

    Update deface.py

--- deface.py
+++ deface.py
@@ -1,16 +1,16 @@
 import os
 
 bsh = '''
-mulai(){
+function mulai(){
 figlet "HACKED"|lolcat
 figlet "BY"|lolcat
 figlet "R3XD"|lolcat
 
-echo -e "\e[31m<========================================>
+echo -e "\e[31m<========================================>"
 echo -e "\e[32mCreated By Someone\e[0m"
 echo -e "\e[31mNo One Cares But\e[0m"
 echo -e "\e[31mWe cares about You\e[0m"
-echo -e "\e[31m<========================================>
+echo -e "\e[31m<========================================>"
 sleep 2
 clear
 mulai
