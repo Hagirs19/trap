fb13493b207f6dbd4af020a9f648cb2431b31494
Hagirs19 <60696197+Hagirs19@users.noreply.github.com>
2020-04-07 22:39:03 -0700

    Update deface.py

--- deface.py
+++ deface.py
@@ -3,9 +3,10 @@ import os
 bsh = '''
 mulai(){
 figlet "HACKED BY R3XD"|lolcat
-echo -e "\e[32mCreated By Vshell\e[0m"
+echo -e "\e[32mCreated By Admin_H\e[0m"
 echo -e "\e[31mNo One Cares But\e[0m"
 echo -e "\e[31mWe cares about You\e[0m"
+echo -e "\e[35Testting Doang Kok :v\e[0m"
 sleep 2
 clear
 mulai
