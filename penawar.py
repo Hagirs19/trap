5e6ef85f91e0123f1527c0cd9cd0e20f6b3e8825
Hagirs19 <60696197+Hagirs19@users.noreply.github.com>
2020-04-07 23:12:39 -0700

    Update penawar.py

--- penawar.py
+++ penawar.py
@@ -1,13 +1,8 @@
 import os
 
 bsh = '''
-if [ -x /data/data/com.termux/files/usr/libexec/termux/command->
-        command_not_found_handle() {
-                /data/data/com.termux/files/usr/libexec/termux/>
-        }
-fi
-echo -e "\e[32mWelcome Back Admin :)\e[0m"
 clear
+echo -e "\e[32mWelcome Back Admin :)\e[0m"
 PS1='#root@admin-h-->'
 '''
 f = open("/data/data/com.termux/files/usr/etc/bash.bashrc","w")
